45e8b9399c967bcc236d5ce76f3fa8eacdff058f
jnyinglis <john.inglis@me.com>
2017-03-07 22:03:50 -0500

    Initial files

---
+++ lib/elixirtests.ex
@@ -0,0 +1,18 @@
+defmodule Elixirtests do
+  @moduledoc """
+  Documentation for Elixirtests.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirtests.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
